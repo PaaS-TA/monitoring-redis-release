49c0e0ce91c90ffd1d891d22ce3121f4e93e7487
hsohans <hso@innogrid.com>
2022-08-03 00:39:07 +0000

    add create-release.sh shell file

---
+++ create-release.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+
+if [ -d ./.dev_builds ]; then
+  echo "delete .dev_builds"
+  rm -rf .dev_builds
+fi
+if [ -d ./dev_releases ]; then
+  echo "delete dev_releases"
+  rm -rf dev_releases
+fi
+if [ -f ./monitoring-redis-release-5.8.0.tgz ]; then
+  echo "delete monitoring-redis-release-5.8.0.tgz"
+  rm -rf monitoring-redis-release-5.8.0.tgz
+fi
+
+#if [ -d ./blobs ]; then
+#  echo "delete blobs"
+#  rm -rf blobs
+#fi
+#
+
+bosh create-release --force --tarball monitoring-redis-release-5.8.0.tgz --name monitoring-redis-release --version 5.8.0
